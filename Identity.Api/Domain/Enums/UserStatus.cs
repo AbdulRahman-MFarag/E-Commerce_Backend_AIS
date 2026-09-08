d4cbe0a2dba290e25a53f5926c44ab8860854473
Abdo-Amin <abdelrhman.amin029@gmail.com>
2026-09-08 08:17:16 +0300

    Added product-service

--- Identity.Api/Domain/Enums/UserStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace Identity.Api.Domain.Enums;
-
-public enum UserStatus
-{
-    Active = 1,
-    Inactive = 2,
-    Suspended = 3
-}
---
+++ ProductService.Application/DTOs/Categories/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductService.Application.DTOs.Categories;
+
+public class CategoryDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+}
